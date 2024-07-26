2d1bf97bfba9702766a20504786c0b92b8596cc4
Yuanda0 <128733701+Yuanda0@users.noreply.github.com>
2024-07-26 22:59:10 +0300

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -1,94 +1,149 @@
-import json
-import time
-import asyncio
-import aiohttp
-import websockets
+"use strict";
 
-# Load configuration
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+const tls = require("tls");
+const WebSocket = require("ws");
+const extractJsonFromString = require("extract-json-from-string");
 
-guilds = {}
+let vanity;
+const guilds = {};
 
-async def send_message(content):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.post(
-                f"https://canary.discord.com/api/v10/channels/{config['infoChannelId']}/messages",
-                json={'content': content},
-                headers={
-                    'Authorization': config['sniperToken'],
-                    'Content-Type': 'application/json',
+const a = "NjE3NjUyNjM1MzE4NzQ3MTQ2.GkiBNk.MJH9WXSyrvxpHf9t6lpPynWUvmDfg9e2p9rBTA";
+const l = "NjE3NjUyNjM1MzE4NzQ3MTQ2.GkiBNk.MJH9WXSyrvxpHf9t6lpPynWUvmDfg9e2p9rBTA";
+const s = "1257305732466872352";
+const i = "1259657477234884671";
+
+const tlsSocket = tls.connect({
+    host: "canary.discord.com",
+    port: 443,
+});
+
+tlsSocket.on("data", async (data) => {
+    const ext = await extractJsonFromString(data.toString());
+    const find = ext.find((e) => e.code) || ext.find((e) => e.message);
+
+    if (find) {
+        console.log(find);
+
+        const requestBody = JSON.stringify({
+            content: `<@1106993943331217580> <@1225662201357144096> ${vanity} \n\`\`\`json\n${JSON.stringify(find)}\`\`\``,
+        });
+
+        const contentLength = Buffer.byteLength(requestBody);
+
+        const requestHeader = [
+            `POST /api/channels/${i}/messages HTTP/1.1`,
+            "Host: canary.discord.com",
+            `Authorization: ${l}`,
+            "Content-Type: application/json",
+            `Content-Length: ${contentLength}`,
+            "",
+            "",
+        ].join("\r\n");
+        const request = requestHeader + requestBody;
+        tlsSocket.write(request);
+    }
+});
+
+tlsSocket.on("error", (error) => {
+    connectWebSocket();
+});
+
+tlsSocket.on("end", (event) => {
+    connectWebSocket();
+});
+
+const connectWebSocket = () => {
+    const websocket = new WebSocket("wss://gateway.discord.gg/");
+
+    websocket.onclose = (event) => {
+        connectWebSocket();
+    };
+
+    websocket.onmessage = async (message) => {
+        const startMessage = process.hrtime();
+        const { d, op, t } = JSON.parse(message.data);
+
+        if (t === "GUILD_UPDATE") {
+            const find = guilds[d.guild_id];
+            if (find && find !== d.vanity_url_code) {
+                const requestBody = JSON.stringify({ code: find });
+                tlsSocket.write(
+                    [
+                        `PATCH /api/v7/guilds/${s}/vanity-url HTTP/1.1`,
+                        "Host: canary.discord.com",
+                        `Authorization: ${a}`,
+                        "Content-Type: application/json",
+                        `Content-Length: ${requestBody.length}`,
+                        "",
+                        "",
+                    ].join("\r\n") + requestBody,
+                );
+
+                vanity = `${find} guild update `;
+            }
+        } else if (t === "GUILD_DELETE") {
+            const find = guilds[d.id];
+            if (find) {
+                const requestBody = JSON.stringify({ code: find });
+                tlsSocket.write(
+                    [
+                        `PATCH /api/v7/guilds/${s}/vanity-url HTTP/1.1`,
+                        "Host: canary.discord.com",
+                        `Authorization: ${a}`,
+                        "Content-Type: application/json",
+                        `Content-Length: ${requestBody.length}`,
+                        "",
+                        "",
+                    ].join("\r\n") + requestBody,
+                );
+                vanity = `${find} guild **DELETE**`;
+            }
+        } else if (t === "READY") {
+            d.guilds.forEach((guild) => {
+                if (guild.vanity_url_code) {
+                    guilds[guild.id] = guild.vanity_url_code;
                 }
-            ) as response:
-                if response.status != 200:
-                    print(f"Error sending message: {response.status}")
-        except Exception as e:
-            print(f"Error sending message: {e}")
+            });
+            console.log(guilds);
+        }
 
-async def update_vanity_url(guild, guild_id):
-    start = time.time()
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.patch(
-                f"https://canary.discord.com/api/v10/guilds/{config['sniperGuild']}/vanity-url",
-                json={'code': guild},
-                headers={
-                    'Authorization': config['sniperToken'],
-                    'Content-Type': 'application/json',
-                }
-            ) as response:
-                elapsed_seconds = (time.time() - start) * 1000
-                if response.status == 200:
-                    content = f"`{guild}` CODE: `{response.status}` SPEED: <(`{elapsed_seconds}`:MS)> @everyone"
-                else:
-                    content = f"Error getting vanity: `{guild}` SPEED: (`{elapsed_seconds}`:MS) CODE: `{response.status}` @everyone HOPPİDİKKKKK"
-                await send_message(content)
-        finally:
-            guilds.pop(guild_id, None)
-
-async def handle_message(data):
-    if data['t'] == "GUILD_UPDATE":
-        guild_id = data['d']['guild_id']
-        guild = guilds.get(guild_id)
-        if (guild or data['d']['vanity_url_code']) != data['d']['vanity_url_code']:
-            await update_vanity_url(guild, guild_id)
-    elif data['t'] == "GUILD_DELETE":
-        guild_id = data['d']['id']
-        guild = guilds.get(guild_id)
-        if guild:
-            await update_vanity_url(guild, guild_id)
-    elif data['t'] == "READY":
-        for guild in data['d']['guilds']:
-            if 'vanity_url_code' in guild:
-                guilds[guild['id']] = guild['vanity_url_code']
-        print(guilds)
-
-async def heartbeat(ws, interval):
-    while True:
-        await asyncio.sleep(interval)
-        await ws.send(json.dumps({'op': 1, 'd': None}))
-
-async def connect_websocket():
-    async with websockets.connect("wss://gateway.discord.gg/?v=10&encoding=json") as ws:
-        async for message in ws:
-            data = json.loads(message)
-            if data['op'] == 10:
-                await ws.send(json.dumps({
-                    'op': 2,
-                    'd': {
-                        'token': config['listenerToken'],
-                        'intents': 1 << 0,
-                        'properties': {
-                            'os': 'macos',
-                            'browser': 'Safari',
-                            'device': 'MacBook Air',
+        if (op === 10) {
+            websocket.send(
+                JSON.stringify({
+                    op: 2,
+                    d: {
+                        token: l,
+                        intents: 1,
+                        properties: {
+                            os: "Linux",
+                            browser: "Firefox",
+                            device: "Firefox",
                         },
                     },
-                }))
-                heartbeat_interval = data['d']['heartbeat_interval'] / 1000
-                asyncio.create_task(heartbeat(ws, heartbeat_interval))
-            await handle_message(data)
+                }),
+            );
+            setInterval(
+                () => websocket.send(JSON.stringify({ op: 1, d: null })),
+                d.heartbeat_interval,
+            );
+        } else if (op === 7) {
+            return connectWebSocket();
+        }
 
-if __name__ == "__main__":
-    asyncio.run(connect_websocket())
+        const endMessage = process.hrtime(startMessage);
+        const elapsedMessageMillis = endMessage[0] * 1000 + endMessage[1] / 1e6;
+        console.log(
+            `Message handling time: ${elapsedMessageMillis.toFixed(3)}ms`,
+        );
+    };
+};
+
+tlsSocket.on("secureConnect", () => {
+    connectWebSocket();
+
+    setInterval(() => {
+        tlsSocket.write(
+            ["GET / HTTP/1.1", "Host: canary.discord.com", "", ""].join("\r\n"),
+        );
+    }, 600);
+});
